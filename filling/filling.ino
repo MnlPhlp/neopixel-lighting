bfdef1690026f788addb30cfec3a36e026cd11af
Manuel Philipp <mannip.31@gmail.com>
2019-07-08 21:16:45 +0200

    fixed zero-width gap

--- filling/filling.ino
+++ filling/filling.ino
@@ -21,6 +21,11 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 1 // Time (in milliseconds) to pause between pixels
 
+
+int space = 2;
+int pause = 0;
+int counting = -1;
+  
 void setup() {
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
   // Any other board, you can remove this part (but no harm leaving it):
@@ -34,22 +39,36 @@ void setup() {
 }
 
 void loop() {
-  int space = 5;
-  int pause = 0;
+  Serial.println("space: "+String(space));  
   int i = 0;
   int count = 0;
-
+  int movingDots = 0;
+  space++; // without this i had to add 1 every time i use the variable
   int maxJ = NUMPIXELS/2*space-1;
+  
   pixels.clear(); // Set all pixel colors to 'off'
-//  for(int i =0; i<300;i++){
-//    pixels.setPixelColor(i,0,125,0);
-//    pixels.setPixelColor(NUMPIXELS-i,0,125,0);
-//    count++;
-//  }
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
+  if (space==1){
+    for(int i=0; i<NUMPIXELS/2; i++){
+      pixels.setPixelColor(i,0,125,0);
+      pixels.setPixelColor(NUMPIXELS-i,0,125,0);
+      pixels.show();
+      delay(pause);
+    }
+    for(int i=NUMPIXELS/2; i>0; i--){
+      pixels.setPixelColor(i,0,0,0);
+      pixels.setPixelColor(NUMPIXELS-i,0,0,0);
+      pixels.show();
+      delay(pause);
+    }
+
+    counting = -counting;
+    return;
+  }
+  
   for(int j=0; j<maxJ;j++) {
-    for(int k=0; k < j/space + 1; k++ ){
+    if(j%space == 0 && movingDots < NUMPIXELS/2) movingDots ++;
+    
+    for(int k=0; k < movingDots; k++ ){
        if ((j-k*space) < (NUMPIXELS/2)-count){
          pixels.setPixelColor(j-k*space,0,125,0);
          pixels.setPixelColor(j-k*space-1,0,0,0);
@@ -66,21 +85,28 @@ void loop() {
     pixels.show();
     delay(pause);
   }
+  
   i=0;
-  int movingDots = 0;
+  movingDots=0;
+  
   for(int j = 0; j<maxJ; j++){
-    if(j%space == 0) movingDots ++;
+    if(j%(space-1) == 0 && movingDots < NUMPIXELS/2) movingDots ++;
    
     for(int k = 0; k<movingDots; k++){
-      pixels.setPixelColor(k*(space+1)-j,0,0,0);
-      pixels.setPixelColor(k*(space+1)-j-1,0,125,0);
+      pixels.setPixelColor(k*space-j,0,0,0);
+      pixels.setPixelColor(k*space-j-1,0,125,0);
 
-      pixels.setPixelColor(NUMPIXELS-(k*(space+1)-j)-1,0,0,0);
-      pixels.setPixelColor(NUMPIXELS-(k*(space+1)-j),0,125,0);
+      pixels.setPixelColor(NUMPIXELS-(k*space-j)-1,0,0,0);
+      pixels.setPixelColor(NUMPIXELS-(k*space-j),0,125,0);
     }
     
     pixels.show();
     delay(pause);
   }
-
+  
+  Serial.println(String(counting));
+  if (space == 11) counting = -counting;
+  space += counting-1;
+  if (space<0) space = 0;
+  
 }
