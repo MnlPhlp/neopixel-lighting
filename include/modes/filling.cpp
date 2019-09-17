16c51a8e310fc82d83bfbb9c64c242d4901ae25c
Manuel Philipp <mannip.31@gmail.com>
2019-09-17 12:15:38 +0200

    startet emptying

--- include/modes/filling.cpp
+++ include/modes/filling.cpp
@@ -6,6 +6,7 @@ namespace modes
     const int space = 5; //space between the points
     int maxI;
     static int stoppedPixels; //count of stoped pixels
+    static bool filling = true;
     static int offset = 0;
 
     if (loopCount > (NUMPIXELS/2)){
@@ -16,25 +17,32 @@ namespace modes
       maxI = loopCount;
     }
 
+    uint32_t c1 = filling ? G_color : 0;
+    uint32_t c2 = filling ? 0 : G_color;
+
     // moving a row of pixels with space
     for (int i = 0; i < maxI; i++)
     {
       if (i%space == offset){
-        myPixels::setPixelColor(i,G_color);
-        myPixels::setPixelColor(i-1,0);
+        myPixels::setPixelColor(i,c1);
+        myPixels::setPixelColor(i-1,c2);
         // same pixel counted from the other side of the led-strip
-        myPixels::setPixelColor(NUMPIXELS-i,G_color);
-        myPixels::setPixelColor(NUMPIXELS-i+1,0);
+        myPixels::setPixelColor(NUMPIXELS-i,c1);
+        myPixels::setPixelColor(NUMPIXELS-i+1,c2);
         if (i == NUMPIXELS/2 - (stoppedPixels+1)){
-          //if a pixel reched the end add 1 to stopped pixels
-          stoppedPixels++;
+          if (filling){
+            //if a pixel reched the end add 1 to stopped pixels
+            stoppedPixels += filling ? 1 : -1;
+          }
         }
       }
     }
  
     myPixels::show();
 
-    offset ++;
+    if (stoppedPixels == NUMPIXELS/2) filling = !filling;
+    offset += filling ? 1 : -1;
     if (offset == space) offset=0;
+    if (offset == -1) offset = space-1;
   }
 } // namespace modes
